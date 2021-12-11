ef3126429114cfb27d93098467aadd3fd5d1d6e7
CedarMan04 <ace.f6f.5.resipuro@gmail.com>
2021-12-12 04:02:00 +0900

    カルーセルメニューの追加

--- src/components/pages/Top.js
+++ src/components/pages/Top.js
@@ -1,12 +1,25 @@
+import { Box, Grid } from '@material-ui/core'
 import React from 'react'
 import Header from '../molecules/Header'
+import LatestWorksList from '../molecules/LatestWorksList'
 import SideBar from '../molecules/SideBar'
 
 const Top = () => {
     return (
         <>
-            <Header />
-            <SideBar />
+        <Box>
+          <Header />
+        </Box>
+        <Box margin={5}>
+            <Grid container spacing={8}>
+                <Grid item xs={3}>
+                    <SideBar />
+                </Grid>
+                <Grid item xs={7}>
+                    <LatestWorksList />
+                </Grid>
+            </Grid>
+        </Box>
         </>
     )
 }
